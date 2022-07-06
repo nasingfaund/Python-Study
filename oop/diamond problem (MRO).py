4ea0c706aa73d9701170e24d7435b02379b9501b
night <night@mail.com>
2022-07-07 00:00:20 +0500

    .

--- oop/diamond problem (MRO).py
+++ oop/diamond problem (MRO).py
@@ -1,35 +1,51 @@
 """
-Порядок разрешения методов
-Method Resolution Order - MRO
+Порядок разрешения методов (Method Resolution Order - MRO)
 """
-
-
 class A:
     def getInfo(self):
         print('A')
-
 
 class B(A):
     def getInfo(self):
         print('B')
 
-
 class C(A):
     def getInfo(self):
         print('C')
 
-
-# если поменять порядок наследования,
-# результат getInfo() изменится
 class D(C, B):
     pass
 
-# MRO - алгоритм поиска в ширину:
-# сначала интерпретатор будет искать метод getInfo
-# в классе C (если наследоваться в порядке C, B) или в B
-# (если наследоваться в порядке B, C), а затем в классе A
-
-
-d = D()
-d.getInfo()
+D().getInfo()
 print(D.mro())
+
+# пример 2 (в этих случаях вывод будет в обратном порядке по MRO)
+
+class A:
+    def __init__(self):
+        print('class A')
+
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print('class B')
+
+
+class C(A):
+    def __init__(self):
+        super().__init__()
+        print('class C')
+
+
+class Test(B, C):
+    def __init__(self):
+        super().__init__()
+        print('class Test')
+
+# вывод будет идти в обратном порядке по mro (см. результат принта mro)
+# то есть в обратном порядке по очереди наследования: Test(B, C)...А
+# получаем линейный вид иерархии: Test <- B <- C <- A
+# вывод в обратном порядке: A -> C -> B -> Test
+Test()
+print(Test.mro())
