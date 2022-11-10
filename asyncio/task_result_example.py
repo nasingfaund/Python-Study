2ec5746327bae34f4a8f63908383796e0dc4b5b0
night <y@main.com>
2022-11-10 21:51:48 +0300

    asyncio practice

--- asyncio/task_result_example.py
+++ asyncio/task_result_example.py
@@ -4,22 +4,26 @@ import asyncio
 async def long_operation(time):
     await asyncio.sleep(time)
     print(f'end of long_operation with {time} seconds')
-    return 'some value'
+    return time
 
 
 async def some_action():
-    print('start some_action...')
     print('end of some_action')
 
 
+# методы вызваны асинхронно => принты выведутся в порядке возрастания времени работы методов
 async def main():
-    results = await asyncio.gather(
-        long_operation(2),
-        long_operation(4),
-        long_operation(1),
-        some_action()
-    )
-    print(results)
+    task1 = asyncio.create_task(long_operation(2))
+    task2 = asyncio.create_task(long_operation(4))
+    task3 = asyncio.create_task(long_operation(1))
+    task4 = asyncio.create_task(some_action())
+    tasks = [task1, task2, task3, task4]
 
+    # просто вызов
+    # await asyncio.wait(tasks)
+
+    # вызов с получением результатов
+    results = await asyncio.gather(*tasks)
+    print(results)
 
 asyncio.run(main())
