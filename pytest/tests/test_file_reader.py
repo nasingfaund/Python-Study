76dedfd2474c195ac0a6e915c9efc3bee98ff2e0
night <vanobel159@gmail.com>
2022-12-26 14:47:30 +0300

    pytest scopes

--- pytest/tests/test_file_reader.py
+++ pytest/tests/test_file_reader.py
@@ -1,6 +1,10 @@
 from functions import write_to_file, read_from_file
 
 
-def test_with_file(flush_file_fixture):
+def test_with_file():
+    """
+    можно не передавать в параметры фикстуру flush_file_fixture,
+    т.к. у нее есть параметр autouse=True (подробнее в conftest.py)
+    """
     write_to_file('text')
     assert read_from_file() == 'text'
